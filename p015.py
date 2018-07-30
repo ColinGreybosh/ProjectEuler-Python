a2c6d4579c4708a38fce9d9d47ea0bf220852351
Colin Greybosh <colingreybosh@gmail.com>
2018-07-30 17:51:16 -0400

    Completed P015

---
+++ p015.py
@@ -0,0 +1,14 @@
+# Starting in the top left corner of a 2×2 grid, and only being able to
+# move to the right and down, there are exactly 6 routes to the bottom
+# right corner.
+#
+# How many such routes are there through a 20×20 grid?
+
+import math
+
+
+def routes_through_grid_of_size_n(n):
+    return int(math.factorial(2 * n) / math.pow((math.factorial(n)), 2))
+
+
+print(routes_through_grid_of_size_n(20))
