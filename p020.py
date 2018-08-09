f15c13e2196f0560602b2a3c02b467016865cb3b
Colin Greybosh <colingreybosh@gmail.com>
2018-08-08 23:38:29 -0400

    Completed problem 20

--- p020.py
+++ p020.py
@@ -7,4 +7,12 @@ n! means n * (n - 1) * ... * 3 * 2 * 1
 Find the sum of the digits in the number 100!
 """
 
+from math import factorial
 
+
+def main():
+    print(sum([int(x) for x in str(factorial(100))]))
+
+
+if __name__ == '__main__':
+    main()
