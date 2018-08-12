6a3b3ab9d565a6c72f8753fc55dd216734918d15
Colin Greybosh <colingreybosh@gmail.com>
2018-08-12 12:26:53 -0400

    Added functions pertaining to factors

--- functions.py
+++ functions.py
@@ -1,12 +1,9 @@
-import math
-
-
 def is_prime(n: int):
     if n == 2:
         return True
     if n <= 1 or is_even(n):
         return False
-    for i in range(3, math.ceil(math.sqrt(n)) + 1, 2):
+    for i in range(3, int(n ** 0.5) + 1, 2):
         if n % i == 0:
             return False
     return True
@@ -16,3 +13,27 @@ def is_even(n: int):
     if n & 1 == 0:
         return True
     return False
+
+
+def find_proper_divisors(n: int):
+    divisors = [1]
+    r = range(2, int(n ** 0.5) + 1)
+    if not is_even(n):
+        r = range(3, int(n ** 0.5) + 1, 2)
+    for i in r:
+        if n % i == 0:
+            divisors.append(i)
+            divisors.append(n // i)
+    return set(divisors)
+
+
+def find_factors(n: int):
+    factors = [1, n]
+    r = range(2, int(n ** 0.5) + 1)
+    if not is_even(n):
+        r = range(3, int(n ** 0.5) + 1, 2)
+    for i in r:
+        if n % i == 0:
+            factors.append(i)
+            factors.append(n // i)
+    return set(factors)
